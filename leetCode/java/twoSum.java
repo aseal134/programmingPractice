097fdd95dac047933026fc51de9e6c66fe9e5f85
Aseal Yousuf <aseal.yousuf@gmail.com>
2015-10-09 20:11:32 -0500

    Passed Two Sum on leetCode

--- leetCode/java/twoSum.java
+++ leetCode/java/twoSum.java
@@ -1,9 +1,9 @@
 /*
-LeetCode
-Problem Name: Two Sum
-Problem Number: 1 
-Programming Language: Java
-*/
+ * LeetCode
+ * Problem Name: Two Sum
+ * Problem Number: 1 
+ * Programming Language: Java
+ */
 
 /*
 |Problem |
@@ -17,3 +17,52 @@ You may assume that each input would have exactly one solution.
 Input: numbers={2, 7, 11, 15}, target=9
 Output: index1=1, index2=2
 */
+
+public class Solution {
+
+	/*
+		Time complexity: O(n)
+		Space:           O(n)
+	*/
+    public int[] twoSum(int[] nums, int target) {
+        
+        int[] sumIndexs = new int[2];
+ 
+        HashMap<Integer,Integer> sumMap = new HashMap<Integer,Integer>();
+        
+        for(int i =0; i<nums.length; i++){
+            if(sumMap.containsKey(nums[i])){
+                sumIndexs[0] = sumMap.get(nums[i]) + 1;
+                sumIndexs[1] = i + 1;
+            }
+            else{
+                sumMap.put(target - nums[i], i);
+            }
+        }
+        return sumIndexs;
+        
+    }
+
+    /*
+		Time complexity: O(n^2)
+		Space:           O(1)        
+	*/
+    public int[] twoSum2(int[] nums, int target) {
+		
+		int[] sumIndexs = new int[2];
+ 
+        for(int i = 0; i < nums.length; i++){
+            for(int j = i+1; j< nums.length; j++){
+                if((nums[i] + nums[j]) == target){
+                    sumIndexs[0] = i + 1;
+                    sumIndexs[1] = j + 1;
+                }
+            }
+        }
+        
+        return sumIndexs;
+        
+    }
+
+
+}
